e4dca64b666f1f60ee4966e3249bd82c891791fc
FalkovskiyAV <avf-91@yandex.ru>
2026-03-29 18:35:29 +0300

    Композиция

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
 import './App.css';
 import Button from './components/Button/Button';
+import CardButton from './components/CardButton/CardButton';
 import JournalItem from './components/JournalItem/JournalItem';
 
 function App() {
-  const data = [
-    {
-      title: 'Подготовка к обновлению курсов',
-      date: new Date(),
-      text: 'Сегодня провёл весь день за...'
-    },
-    {
-      title: 'Поход в годы',
-      date: new Date(),
-      text: 'Думал, что очень много време...'
-    },
-    {
-      title: 'Первая заметка',
-      date: new Date(),
-      text: 'Создал первую заметку, чтобы ...'
-    }
-  ];
+	const data = [
+		{
+			title: 'Подготовка к обновлению курсов',
+			date: new Date(),
+			text: 'Сегодня провёл весь день за...'
+		},
+		{
+			title: 'Поход в годы',
+			date: new Date(),
+			text: 'Думал, что очень много време...'
+		},
+		{
+			title: 'Первая заметка',
+			date: new Date(),
+			text: 'Создал первую заметку, чтобы ...'
+		}
+	];
 
-  return (
-    <div>
-      <h1>Заголовок 1</h1>
-      <p>Какой-то текст</p>
-      <Button />
-      <JournalItem title={data[0].title} date={data[0].date} text={data[0].text} />
-      <JournalItem title={data[1].title} date={data[1].date} text={data[1].text} />
-      <JournalItem title={data[2].title} date={data[2].date} text={data[2].text} />
-    </div>
-  );
+	return (
+		<div>
+			<h1>Заголовок 1</h1>
+			<p>Какой-то текст</p>
+			<Button />
+			<CardButton>Новое воспоминание</CardButton>
+			{data.map((el, index) => (
+				<CardButton key={index}>
+					<JournalItem title={el.title} date={el.date} text={el.text} />
+				</CardButton>
+			))}
+		</div>
+	);
 }
 
 export default App;
