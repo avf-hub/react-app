2c45e3c65615e9a7036ac41111ee35b84b27957e
FalkovskiyAV <avf-91@yandex.ru>
2026-04-01 22:41:08 +0300

    Исправление вычисления id записи списка

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
 			title: item.title,
 			text: item.text,
 			date: new Date(item.date),
-			id: Math.max(...oldItems.map(it => it.id)) + 1
+			id: oldItems.length > 0 ? Math.max(...oldItems.map(it => it.id)) + 1 : 1
 		}]);
 	};
 
