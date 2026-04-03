7af9b692a263395f0faa395493e9f7a2dc48bf1b
FalkovskiyAV <avf-91@yandex.ru>
2026-04-03 17:11:47 +0300

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,21 @@ import Header from './components/Header/Header';
 import JournalList from './components/JournalList/JournalList';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 import JournalForm from './components/JournalForm/JournalForm';
-import { useState } from 'react';
-
-const INITIAL_DATA = [
-	// {
-	// 	id: 1,
-	// 	title: 'Подготовка к обновлению курсов',
-	// 	date: new Date(),
-	// 	text: 'Сегодня провёл весь день за книгами'
-	// },
-	// {
-	// 	id: 2,
-	// 	title: 'Поход в годы',
-	// 	date: new Date(),
-	// 	text: 'Думал, что очень много времени'
-	// }
-];
+import { useEffect, useState } from 'react';
 
 function App() {
-	const [items, setItems] = useState(INITIAL_DATA);
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		const storedData = localStorage.getItem('data');
+		if (storedData) {
+			const data = JSON.parse(storedData);
+			setItems(data.map(item => ({
+				...item,
+				date: new Date(item.date)
+			})));
+		}
+	}, []);
 
 	const addItem = item => {
 		setItems(oldItems => [...oldItems, {
